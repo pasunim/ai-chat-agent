5c9a6b82effab4ba15340ebe963896e03f51bf15
Pasu Nimsuwan <49263096+pasunim@users.noreply.github.com>
2024-06-29 08:28:50 +0700

    updated

--- streamlit.py
+++ streamlit.py
@@ -4,8 +4,11 @@ import pandas as pd
 import streamlit as st
 import requests
 from io import BytesIO
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
-genai.configure(api_key="AIzaSyAnpJzcco3B4uOFoTNzKu15bQc8URMFKJY")
+genai.configure(api_key=API_KEY)
 
 generation_config = {
     "temperature": 0.1,
